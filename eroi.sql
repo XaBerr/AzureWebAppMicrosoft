a487c8193162bc52b4252ea61438aff2d1840d95
Federico Berra <fede.berra@hotmail.it>
2016-12-02 12:38:14 +0100

    Removed code that make error in Azure

--- eroi.sql
+++ eroi.sql
@@ -1,24 +1,6 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : LOCAL
-Source Server Version : 50615
-Source Host           : 127.0.0.1:3306
-Source Database       : eroi
-
-Target Server Type    : MYSQL
-Target Server Version : 50615
-File Encoding         : 65001
-
-Date: 2016-11-22 19:37:46
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
 -- Table structure for `eroi`
 -- ----------------------------
-DROP TABLE IF EXISTS `eroi`;
 CREATE TABLE `eroi` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(250) DEFAULT NULL,
